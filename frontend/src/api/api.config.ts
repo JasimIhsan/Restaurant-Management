5cd8d3afd87397615f8981de1b3a70598b89a6e0
Jasim Ihsan M <jasimihsan1234@gmail.com>
2025-09-30 18:51:32 +0530

    fix: .env added in frontend

--- frontend/src/api/api.config.ts
+++ frontend/src/api/api.config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5002/api/';
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
    baseURL: baseUrl,
